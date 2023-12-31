025a9f39268513cfc30653da11625576e5e39625
Maternykh <maternyh.egor@yandex.ru>
2023-12-31 16:29:12 +0500

    first commit

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -3,7 +3,7 @@ import { User } from "../models/userModal.js";
 import jwt from "jsonwebtoken";
 import { registerValidation } from "../validations/auth.js";
 import { validationResult } from "express-validator";
-import bcrypt from "bcrypt";
+
 const router = express.Router();
 router.post("/login", async (req, res) => {
   try {
@@ -13,10 +13,8 @@ router.post("/login", async (req, res) => {
         message: "Пользователь не найден",
       });
     }
-    const isValidPass = await bcrypt.compare(
-      req.body.userPasswordHash,
-      user._doc.userPasswordHash
-    );
+    const isValidPass =
+      req.body.userPasswordHash === user._doc.userPasswordHash;
     if (!isValidPass) {
       return res.status(403).json({
         message: "Неверный логин или пароль",
@@ -65,12 +63,10 @@ router.post("/register", registerValidation, async (req, res) => {
     if (!errors.isEmpty()) {
       return res.status(400).json(errors.array());
     }
-    const password = req.body.userPasswordHash;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+
     const doc = new User({
       userEmail: req.body.userEmail,
-      userPasswordHash: passwordHash,
+      userPasswordHash: req.body.userPasswordHash,
       userAvatar: req.body.userAvatar,
     });
     const user = await doc.save();
